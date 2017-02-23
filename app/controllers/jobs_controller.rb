80bded120c7f6cf70eb9eb6a319c23aabf4c46b6
railsroger <railsroger@gmail.com>
2017-02-22 22:42:03 -0800

    Add categories to jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,12 @@
 class JobsController < ApplicationController
   before_action :find_job, only: [:show, :edit, :update, :destroy]
   def index
-    @jobs = Job.all.order("created_at DESC")
+    if params[:category].blank?
+      @jobs = Job.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
@@ -33,11 +38,13 @@ class JobsController < ApplicationController
   end
 
   def destroy
+    @job.destroy
+    redirect_to root_path
   end
 
   private
     def jobs_params
-      params.require(:job).permit(:title, :description, :company, :url, :contact)
+      params.require(:job).permit(:title, :description, :company, :url, :contact, :category_id)
     end
 
     def find_job
